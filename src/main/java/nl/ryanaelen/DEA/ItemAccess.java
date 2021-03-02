6e95644da753a26da502f00015fcc968c6055130
Ryan Aelen <rmm.aelen@student.han.nl>
2021-03-02 11:12:19 +0100

    opdracht 9

--- src/main/java/nl/ryanaelen/DEA/ItemAccess.java
+++ src/main/java/nl/ryanaelen/DEA/ItemAccess.java
@@ -4,15 +4,13 @@ package nl.ryanaelen.DEA;
 import nl.ryanaelen.DEA.services.ItemService;
 import nl.ryanaelen.DEA.services.dto.ItemDTO;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ejb.Singleton;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 @Path("/items")
+@Singleton
 public class ItemAccess {
     private ItemService itemService;
 
@@ -28,17 +26,25 @@ public class ItemAccess {
         return Response.ok(itemService.getAll()).build();
     }
 
-    @GET
-    @Path("/")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response iets() {
-        return Response.ok("bread, butter").build();
-    }
+//    @GET
+//    @Path("/")
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public Response iets() {
+//        return Response.ok("bread, butter").build();
+//    }
 
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response itemId(@PathParam("id") int id) {
         return Response.ok(itemService.getItem(id)).build();
+    }
+
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addItem(ItemDTO itemDTO) {
+        itemService.addItem(itemDTO);
+        return Response.status(Response.Status.CREATED).build();
     }
 }
