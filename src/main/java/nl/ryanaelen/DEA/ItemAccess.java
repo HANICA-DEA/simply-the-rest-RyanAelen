fbec3a8be7dac0c253900bddae732eca2df8ab20
Ryan Aelen <rmm.aelen@student.han.nl>
2021-03-02 10:45:35 +0100

    opdracht 8

--- src/main/java/nl/ryanaelen/DEA/ItemAccess.java
+++ src/main/java/nl/ryanaelen/DEA/ItemAccess.java
@@ -6,6 +6,7 @@ import nl.ryanaelen.DEA.services.dto.ItemDTO;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -32,5 +33,12 @@ public class ItemAccess {
     @Produces(MediaType.TEXT_PLAIN)
     public Response iets() {
         return Response.ok("bread, butter").build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response itemId(@PathParam("id") int id) {
+        return Response.ok(itemService.getItem(id)).build();
     }
 }
